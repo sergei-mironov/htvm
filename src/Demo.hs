65d0fdab2354a45471563936f5b698e915204679
Sergey Mironov <grrwlf@gmail.com>
2018-10-16 01:23:01 +0300

    Demo fixes

--- src/Demo.hs
+++ src/Demo.hs
@@ -8,10 +8,14 @@ main = do
   return ()
 
 demo :: IO ()
-demo = do
+demo =
+  let
+    s = shape [20]
+  in do
   Module <$> pure "vecadd" <$> sequence [
-      function "vecadd" [("A",float32,shape [20]), ()]
+      function "vecadd" [("A",float32,s),("B",float32,s)] $ \[a,b] -> do
+        c <- compute s $ \[i] -> a![i] .+ b![i]
+        d <- compute [s!0,s!0] $ \[i,j] -> c![i,i] .* c![i,i]
+        e <- call "topi.relu" args [d]
+        return e
     ]
-  function
-  runStmtT $ do
-    function
